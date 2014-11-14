7275484cd0a5fa22c43689df1b88f6e03ea81df3
Nicholas Carter <nicholascarter100@gmail.com>
2014-11-14 15:51:32 -0600

    Directing working, and removed unused imports

--- Vid-It/src/com/example/vid_it/AndroidCamera.java
+++ Vid-It/src/com/example/vid_it/AndroidCamera.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,7 +15,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
--- Vid-It/src/com/example/vid_it/TOSActivity.java
+++ Vid-It/src/com/example/vid_it/TOSActivity.java
@@ -2,8 +2,6 @@ package com.example.vid_it;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class TOSActivity extends Activity { 
 	
